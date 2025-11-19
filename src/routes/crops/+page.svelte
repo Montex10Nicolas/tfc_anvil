030f51cf82818ca1ed2441d5d5ddba0480f4414c
Nicolas10Montex <montexdev@protonmail.com>
2025-11-19 08:23:55 +0100

    sort start

--- src/routes/crops/+page.svelte
+++ src/routes/crops/+page.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
 	import * as crop from '$lib/jsons/crops.json';
 
+	type Order =
+		| 'none'
+		| 'name'
+		| 'temperature-min'
+		| 'temperature-max'
+		| 'hydration-min'
+		| 'hydration-max'
+		| 'nutrient';
+
 	interface Crop {
 		name: string;
 		temperature: {
@@ -14,6 +23,62 @@
 		nutrient: 'Nitrogen' | 'Phosphorus' | 'Potassium';
 	}
 	const { crops } = crop as { crops: Crop[] };
+
+	let order: Order = $state('none');
+	let upOrDown: 'asc' | 'desc' = $state('asc');
+
+	let sortedCrops = $derived.by(() => {
+		let arr = [...crops];
+		if (order === 'none') return crops;
+		switch (order) {
+			case 'temperature-min':
+				arr = arr.sort((a, b) => {
+					if (upOrDown === 'asc') {
+						return a.temperature.min - b.temperature.min;
+					} else {
+						return b.temperature.min - a.temperature.min;
+					}
+				});
+				return arr;
+			case 'temperature-max':
+				arr = arr.sort((a, b) => {
+					if (upOrDown === 'asc') {
+						return a.temperature.max - b.temperature.max;
+					} else {
+						return b.temperature.max - a.temperature.max;
+					}
+				});
+				return arr;
+			case 'hydration-min':
+				arr = arr.sort((a, b) => {
+					if (upOrDown === 'asc') {
+						return a.hydration.min - b.hydration.min;
+					} else {
+						return b.hydration.min - a.hydration.min;
+					}
+				});
+				return arr;
+			case 'hydration-max':
+				arr = arr.sort((a, b) => {
+					if (upOrDown === 'asc') {
+						return a.hydration.max - b.hydration.max;
+					} else {
+						return b.hydration.max - a.hydration.max;
+					}
+				});
+				return arr;
+			case 'nutrient':
+				arr = arr.sort((a, b) => {
+					return a.nutrient > b.nutrient ? 1 : a.nutrient === b.nutrient ? 0 : -1;
+				});
+			default:
+				return arr;
+		}
+	});
+
+	function switchUpDown() {
+		upOrDown = upOrDown === 'asc' ? 'desc' : 'asc';
+	}
 </script>
 
 <main class="w-screen">
@@ -24,22 +89,51 @@
 				<td class="flex flex-col">
 					<div>temperature</div>
 					<div class="grid grid-cols-2">
-						<span>Min</span>
-						<span>Max</span>
+						<button
+							class="cursor-pointer"
+							onclick={() => {
+								order = 'temperature-min';
+								switchUpDown();
+							}}>Min</button
+						>
+						<button
+							class="cursor-pointer"
+							onclick={() => {
+								order = 'temperature-max';
+								switchUpDown();
+							}}>Max</button
+						>
 					</div>
 				</td>
 				<td class="flex flex-col">
 					<div>Hydration</div>
 					<div class="grid grid-cols-2">
-						<span>Min</span>
-						<span>Max</span>
+						<button
+							class="cursor-pointer"
+							onclick={() => {
+								order = 'hydration-min';
+								switchUpDown();
+							}}>Min</button
+						>
+						<button
+							class="cursor-pointer"
+							onclick={() => {
+								order = 'hydration-max';
+								switchUpDown();
+							}}>Max</button
+						>
 					</div>
 				</td>
-				<td>Nutrient</td>
+				<td
+					onclick={() => {
+						order = 'nutrient';
+						switchUpDown();
+					}}>Nutrient</td
+				>
 			</tr>
 		</thead>
 		<tbody class="m-4">
-			{#each crops as { name, hydration, nutrient, temperature }}
+			{#each sortedCrops as { name, hydration, nutrient, temperature }}
 				<tr
 					class="m-4 grid grid-cols-4 items-center justify-between text-center font-semibold capitalize"
 				>
