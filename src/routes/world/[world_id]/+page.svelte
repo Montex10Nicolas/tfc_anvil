0a15799a070ee16bd38fc70c7d166ab98cdf204d
Nicolas10Montex <montexdev@protonmail.com>
2025-12-01 17:54:30 +0100

    minor ui in metals select

--- src/routes/world/[world_id]/+page.svelte
+++ src/routes/world/[world_id]/+page.svelte
@@ -6,17 +6,17 @@
 	const worldID = page.params.world_id;
 </script>
 
-<main class="flex h-screen w-screen items-center justify-center bg-black/80">
+<main class="h-screen w-screen bg-black/80">
 	{#if metals.loading}
 		<p>Loading...</p>
 	{:else if metals.error}
 		<p>Error...</p>
 	{:else if metals.current}
-		<div class="grid grid-cols-3 gap-4">
+		<div class="grid grid-cols-2 gap-4 p-4 md:grid-cols-3 lg:grid-cols-6">
 			{#each metals.current as metal}
 				<a href={`/world/${worldID}/metal/${metal.id}`}>
 					<div
-						class="h-48 w-48 place-content-center rounded-2xl bg-white text-center text-sm font-bold"
+						class="h-24 w-48 place-content-center rounded-2xl bg-white text-center text-sm font-bold"
 					>
 						<p class="text-2xl">{metal.name}</p>
 					</div>
